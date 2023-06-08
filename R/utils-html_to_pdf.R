98c2899b7fad34a2af03c0f0796142a72bf5c85b
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-08 11:19:57 +0100

    Simplify browser checking

--- R/utils-html_to_pdf.R
+++ R/utils-html_to_pdf.R
@@ -11,7 +11,7 @@ html_to_pdf <- function(input, output = fs::path_ext_set(aio, "pdf"), ...) {
   invisible(file.exists(output))
 }
 
-try_chrome_print <- function(input, output, format, ...) {
+try_chrome_print <- function(input, output, ...) {
   tryCatch({
     pagedown::chrome_print(input = input, output = output, format = "pdf", ...)
   }, error = function(e) {
@@ -23,8 +23,7 @@ try_chrome_print <- function(input, output, format, ...) {
 }
 
 check_chrome_available <- function() {
-  browser <- pagedown::find_chrome()
-  if (!file.exists(browser)) browser <- Sys.which(browser)
+  browser <- find_browser()
   chrome_available <- utils::file_test("-x", browser)
   if (!chrome_available) {
     cli::cli_warn(c(
@@ -34,4 +33,12 @@ check_chrome_available <- function() {
     ))
   }
   chrome_available
+}
+
+find_browser <- function() {
+  ## Adapted from https://github.com/rstudio/pagedown/blob/466c1c1e8fc4a679aeff25bdd19fd834c0b78bbd/R/chrome.R#LL78C3-L82C4
+  if (is.na(browser <- Sys.getenv("PAGEDOWN_CHROME", NA))) {
+    browser <- pagedown::find_chrome()
+  }
+  browser
 }
