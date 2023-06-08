944f6de73670d18ba086b12fcc50f980a015d71b
Milan Malfait <m.malfait@ucl.ac.uk>
2023-06-08 10:53:34 +0100

    Refactor `html_to_pdf` and improve warnings

--- R/utils-html_to_pdf.R
+++ R/utils-html_to_pdf.R
@@ -1,14 +1,37 @@
-html_to_pdf <- function(input, output) {
+html_to_pdf <- function(input, output = fs::path_ext_set(aio, "pdf"), ...) {
   rlang::check_installed("pagedown")
 
+  chrome_available <- check_chrome_available()
+  if (!chrome_available) {
+    return(invisible(FALSE))
+  } else {
+    cli::cli_text("Converting '{.file {basename(input)}}' to '{.file {basename(output)}}'")
+    try_chrome_print(input = input, output = output, ...)
+  }
+  invisible(file.exists(output))
+}
+
+try_chrome_print <- function(input, output, format, ...) {
   tryCatch({
-    cli::cli_text("Writing '{.file {basename(output)}}'")
-    pagedown::chrome_print(input = input, output = output, format = "pdf")
+    pagedown::chrome_print(input = input, output = output, format = "pdf", ...)
   }, error = function(e) {
     cli::cli_warn(c(
       "x" = "chrome_print failed to write {.file {basename(output)}} with the following error:",
-      " " = "{.emph '{e$message}'}",
-      "i" = "Do you have Chrome installed?"
+      " " = "{.emph '{e$message}'}"
     ))
   })
+}
+
+check_chrome_available <- function() {
+  browser <- pagedown::find_chrome()
+  if (!file.exists(browser)) browser <- Sys.which(browser)
+  chrome_available <- utils::file_test("-x", browser)
+  if (!chrome_available) {
+    cli::cli_warn(c(
+      "x" = "Chrome is not available on your system.",
+      " " = "Please install Chrome and try again.",
+      "i" = "See https://www.google.com/chrome/ for more information."
+    ))
+  }
+  chrome_available
 }
