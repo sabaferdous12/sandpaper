0df13db34d18968e7180f04ebc37de1efcd2b3c1
Milan Malfait <m.malfait@ucl.ac.uk>
2023-07-18 14:35:22 +0100

    Enable ipynb test for `build_lesson()` again

--- tests/testthat/test-build_lesson.R
+++ tests/testthat/test-build_lesson.R
@@ -442,12 +442,11 @@ test_that("episodes with HTML in the title are rendered correctly", {
   )))
 })
 
+test_that("build_lesson also creates jupyter notebooks when required", {
+  ## Re-create lesson with notebooks enabled
+  tmp <- local_lesson(ipynb = TRUE)
+  build_lesson(tmp, quiet = TRUE, preview = FALSE)
 
-# test_that("build_lesson also creates jupyter notebooks when required", {
-#   ## Re-create lesson with notebooks enabled
-#   tmp <- local_lesson(ipynb = TRUE)
-#   build_lesson(tmp, quiet = TRUE, preview = FALSE)
-
-#   built_path <- path_built(tmp)
-#   expect_true(fs::file_exists(fs::path(built_path, "introduction.ipynb")))
-# })
+  built_path <- path_built(tmp)
+  expect_true(fs::file_exists(fs::path(built_path, "introduction.ipynb")))
+})
